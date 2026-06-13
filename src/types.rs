2df591733fba88474015319e33081cf51a98e3fa
Matteo Zeccoli Marazzini <teo.zec@hotmail.it>
2026-06-13 18:12:32 +0200

    feat: add basic types and lib

---
+++ src/types.rs
@@ -0,0 +1,65 @@
+pub type DomainName = Vec<Vec<u8>>;
+
+#[derive(Debug, Clone, Copy)]
+pub enum Opcode {
+    Standard,
+    Inverse,
+    Status,
+    Unknown(u8),
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum ResponseCode {
+    NoError,
+    FormatError,
+    ServerFailure,
+    NameError,
+    NotImplemented,
+    Refused,
+    Unknown(u8),
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum RType {
+    A,
+    NS,
+    MD,
+    MF,
+    CNAME,
+    SOA,
+    MB,
+    MG,
+    MR,
+    NULL,
+    WKS,
+    PTR,
+    HINFO,
+    MINFO,
+    MX,
+    TXT,
+    Unknown(u16),
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum QType {
+    RType(RType),
+    AXFR,
+    MAILB,
+    MAILA,
+    Any,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum RClass {
+    IN,
+    CS,
+    CH,
+    HS,
+    Unknown(u16),
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum QClass {
+    RClass(RClass),
+    Any,
+}
